057998afb3b02fb610f5efc8047f7bfa0ae6793f
Martins Nelson <nelsontecti@gmail.com>
2022-06-11 20:13:16 -0300

    Remove https

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace Qrcode_API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Qrcode_API v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
